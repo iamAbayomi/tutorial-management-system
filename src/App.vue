7133ca723ff637329d91deb2e5b81dbf2df06c42
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-06-17 02:34:43 +0100

    Added main app succesfully

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+          <a href="#" class="navbar-brand">Fola </a>
+          <div class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a href="/tutorials" class="nav-link">Tutorials</a>
+            </li>
+            <li class="nav-item">
+              <a href="/add" class="nav-link">Add </a>
+            </li>
+          </div>
+        </nav>
+      
+        <div class="container mt-3">
+
+          <router-view />
+
+        
+        </div>
+
+    </div> 
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
